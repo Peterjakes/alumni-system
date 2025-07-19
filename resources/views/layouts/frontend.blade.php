9e9e642df21569a57aa95806a5ea91a571677df5
Peter Jakes <kiberujakes@gmail.com>
2025-07-19 20:17:18 +0300

    added link to settings page in frontend dashboard

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -9,18 +9,19 @@
 <body class="bg-gray-100 font-sans leading-normal tracking-normal">
 
     <nav class="bg-blue-700 text-white p-4">
-        <div class="container mx-auto flex justify-between">
-            <div class="text-lg font-semibold">🎓 Alumni System</div>
-            <div>
-                <a href="{{ route('dashboard') }}" class="hover:underline">Dashboard</a>
-                <a href="{{ route('profile.edit') }}" class="ml-4 hover:underline">Profile</a>
-                <form method="POST" action="{{ route('logout') }}" class="inline ml-4">
-                    @csrf
-                    <button type="submit" class="hover:underline">Logout</button>
-                </form>
-            </div>
+    <div class="container mx-auto flex justify-between">
+        <div class="text-lg font-semibold">🎓 Alumni System</div>
+        <div>
+            <a href="{{ route('dashboard') }}" class="hover:underline">Dashboard</a>
+            <a href="{{ route('profile.edit') }}" class="ml-4 hover:underline">Profile</a>
+            <a href="{{ route('settings.index') }}" class="ml-4 hover:underline">Settings</a>
+            <form method="POST" action="{{ route('logout') }}" class="inline ml-4">
+                @csrf
+                <button type="submit" class="hover:underline">Logout</button>
+            </form>
         </div>
-    </nav>
+    </div>
+</nav>
 
     <div class="container mx-auto mt-8">
         @yield('content')
