8d901875312866c7e080ce1bc43d61700a1775cb
Peter Jakes <kiberujakes@gmail.com>
2025-07-21 11:20:00 +0300

    created a mail for eventregistration confirmation

---
+++ app/Mail/EventRegistrationConfirmation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Event;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class EventRegistrationConfirmation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $event;
+
+    public function __construct(Event $event)
+    {
+        $this->event = $event;
+    }
+
+    public function build()
+    {
+        return $this->subject('Event Registration Confirmed')
+                    ->view('emails.registration_confirmation');
+    }
+}
