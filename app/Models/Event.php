3a5077d41cec348fd1c219b7346b8e764937ba04
Peter Jakes <kiberujakes@gmail.com>
2025-07-21 09:32:00 +0300

    defined many-to-many relationship between users and events

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User; 
 
 class Event extends Model
 {
     use HasFactory;
+
+    /**
+     * Many-to-Many: An event can have many users (alumni) registered
+     */
+    public function attendees()
+    {
+        return $this->belongsToMany(User::class, 'event_user');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Event; 
 
 class User extends Authenticatable
 {
@@ -42,4 +43,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Many-to-Many: A user can register for many events
+     */
+    public function registeredEvents()
+    {
+        return $this->belongsToMany(Event::class, 'event_user');
+    }
 }
