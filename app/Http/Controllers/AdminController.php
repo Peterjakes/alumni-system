c2aeaafcaa5fc18997f0f5895473ecf9a25da12f
Peter Jakes <kiberujakes@gmail.com>
2025-07-21 12:10:00 +0300

    added logic to AdminController to compute basic dashboard stats

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,4 +10,13 @@ class AdminController extends Controller
     {
         return view('admin.dashboard');
     }
+
+    public function dashboard()
+{
+    $usersCount = \App\Models\User::count();
+    $eventsCount = \App\Models\Event::count();
+    $registrationsCount = \App\Models\EventRegistration::count();
+
+    return view('backend.admin.dashboard', compact('usersCount', 'eventsCount', 'registrationsCount'));
+}
 }
